6dabf1d71eac10d9e01f983db3c460db225d464d
Navas Kurian <navaskurian@Navass-Air.attlocal.net>
2024-05-09 16:06:53 -0400

    Screrners added

--- src/main/scala/NasdaqMarketDataApp.scala
+++ src/main/scala/NasdaqMarketDataApp.scala
@@ -6,11 +6,12 @@ import scala.Console.println
 
 object NasdaqMarketDataApp {
   def main(args: Array[String]): Unit = {
-    val today = "2024-05-03"
+    val yesterday = "2024-05-07"
+    val today = "2024-05-08"
     val spark = SparkSession.builder
       .appName("Simple Application")
       .config("spark.master", "local")
-      .config("spark.driver.memory","2g")
+      .config("spark.driver.memory", "2g")
       .master("local[4]")
       .getOrCreate()
 
@@ -22,13 +23,17 @@ object NasdaqMarketDataApp {
       .load()
 
     import spark.implicits._
-    val nasdaq_summary = nasdaq_summary_raw.withColumn("date1", to_date(col("date"), "yyyyMMdd"))
+    val nasdaq_summary = nasdaq_summary_raw.withColumn("date1", to_date(col("date"), "yyyyMMdd")).drop("date")
     val ma10WindowSpec = Window.partitionBy($"symbol").orderBy($"date1".asc).rowsBetween(-9, 0)
     val ma20WindowSpec = Window.partitionBy($"symbol").orderBy($"date1".asc).rowsBetween(-19, 0)
     val ma50WindowSpec = Window.partitionBy($"symbol").orderBy($"date1".asc).rowsBetween(-49, 0)
+    val monthWindowSpec = Window.partitionBy($"symbol").orderBy($"date1".asc).rowsBetween(-29, 0)
+    val twoMonthWindowSpec = Window.partitionBy($"symbol").orderBy($"date1".asc).rowsBetween(-59, 0)
+    val threeMonthWindowSpec = Window.partitionBy($"symbol").orderBy($"date1".asc).rowsBetween(-89, 0)
+    val sixMonthWindowSpec = Window.partitionBy($"symbol").orderBy($"date1".asc).rowsBetween(-179, 0)
     val range20WindowSpec = Window.partitionBy($"symbol").orderBy($"date1".asc).rowsBetween(-19, 0)
     val nasdaq_summary_df =
-      nasdaq_summary.drop("date")
+      nasdaq_summary
         .withColumn("ma10", avg($"close".cast("Double")).over(ma10WindowSpec))
         .withColumn("ma10", round($"ma10", 2))
         .withColumn("ma20", avg($"close".cast("Double")).over(ma20WindowSpec))
@@ -44,42 +49,132 @@ object NasdaqMarketDataApp {
         .withColumn("adr", (($"avgRange".cast("Double") / 20) - 1) * 100)
         .withColumn("min10", min($"close").over(ma10WindowSpec))
         .withColumn("max10", max($"close").over(ma10WindowSpec))
+        .withColumn("pct10d", (($"max10" - $"min10") / $"min10") * 100)
+        .withColumn("min20", min($"close").over(ma20WindowSpec))
+        .withColumn("max20", max($"close").over(ma20WindowSpec))
+        .withColumn("pct20d", (($"max20" - $"min20") / $"min20") * 100)
+        .withColumn("min30", min($"close").over(monthWindowSpec))
+        .withColumn("max30", max($"close").over(monthWindowSpec))
+        .withColumn("pct30d", (($"max30" - $"min30") / $"min30") * 100)
+        .withColumn("min60", min($"close").over(twoMonthWindowSpec))
+        .withColumn("max60", max($"close").over(twoMonthWindowSpec))
+        .withColumn("pct60d", (($"max60" - $"min60") / $"min60") * 100)
+        .withColumn("min90", min($"close").over(threeMonthWindowSpec))
+        .withColumn("max90", max($"close").over(threeMonthWindowSpec))
+        .withColumn("pct90d", (($"max90" - $"min90") / $"min90") * 100)
+        .withColumn("min180", min($"close").over(sixMonthWindowSpec))
+        .withColumn("max180", max($"close").over(sixMonthWindowSpec))
+        .withColumn("pct180d", (($"max180" - $"min180") / $"min180") * 100)
         .withColumn("symbolToCopy", concat(lit("NASDAQ:"), $"symbol", lit(",")))
         .filter($"dollarVolume" > 500000 and $"date1" === today)
-    nasdaq_summary_df.show(10)
-    println("Total count greater than 500000 dollar volume ", nasdaq_summary_df.count())
-//    val filtered_nasdaq_summary_df = nasdaq_summary_df
-//      .filter($"close" >= $"ma10" && $"close" >= $"ma20" && $"close" >= $"ma50")
-//      .filter($"close" >= 5)
-//    println("Total stocks close greater than all moving averages ", filtered_nasdaq_summary_df.count())
-//
-//    filtered_nasdaq_summary_df.coalesce(1)
-//      .write.format("com.databricks.spark.csv")
-//      .mode("overwrite")
-//      .option("header", "true")
-//      .save("nasdaq_gt_ma")
-//
-//    filtered_nasdaq_summary_df.filter($"adr" > 4)
-//      .coalesce(1)
-//      .write.format("com.databricks.spark.csv")
-//      .mode("overwrite")
-//      .option("header", "true")
-//      .save("nasdaq_adr_gt_four")
+        .cache()
 
-    //val ma10MinMaxWindowSpec = Window.partitionBy($"symbol", $"close").orderBy($"date1".asc).rowsBetween(-9, 0)
+    val move10Df = nasdaq_summary_df
+      .filter(($"close" > ($"min10" + ($"min10" * 0.10))) && ($"pct10d" > 20) && ($"adr" >= 4) && ($"close" >= 5))
+    val universeDf_Narrow = nasdaq_summary_df
+      .filter(($"stockpctchange" > -1 && $"stockpctchange" < 1) && ($"adr" >= 4) && ($"close" >= 5))
     val move20Df = nasdaq_summary_df
-      .withColumn("pct10d", (($"max10" - $"min10") / $"min10") * 100)
-      .filter(($"close" > ($"min10" + ($"min10" * 0.10))) && ($"pct10d" > 20) && ($"adr" >=4) && ($"close" >= 5))
+      .filter(($"close" > ($"min20" + ($"min20" * 0.10))) && ($"pct20d" > 20) && ($"adr" >= 4) && ($"close" >= 5))
+    val move30Df = nasdaq_summary_df
+      .filter(($"close" > ($"min30" + ($"min30" * 0.30))) && ($"pct30d" > 30) && ($"adr" >= 4) && ($"close" >= 5))
+    val move60Df = nasdaq_summary_df
+      .filter(($"close" > ($"min60" + ($"min60" * 0.20))) && ($"pct60d" > 30) && ($"adr" >= 4) && ($"close" >= 5))
+    val move90Df = nasdaq_summary_df
+      .filter(($"close" > ($"min90" + ($"min90" * 0.40))) && ($"pct90d" > 40) && ($"adr" >= 4) && ($"close" >= 5))
+    val move180Df = nasdaq_summary_df
+      .filter(($"close" > ($"min180" + ($"min180" * 0.50))) && ($"pct180d" > 50) && ($"adr" >= 4) && ($"close" >= 5))
 
-    move20Df.show(10)
-    move20Df.filter($"symbol".equalTo("TSLA")).show()
-
-    move20Df
+    move10Df
       .coalesce(1)
       .write.format("com.databricks.spark.csv")
       .mode("overwrite")
       .option("header", "true")
-      .save("nasdaq_ma20_move")
+      .save("nasdaq_ma10_move")
+
+    move30Df
+      .coalesce(1)
+      .write.format("com.databricks.spark.csv")
+      .mode("overwrite")
+      .option("header", "true")
+      .save("nasdaq_ma30_move")
+
+    move90Df
+      .coalesce(1)
+      .write.format("com.databricks.spark.csv")
+      .mode("overwrite")
+      .option("header", "true")
+      .save("nasdaq_ma90_move")
+
+    move180Df
+      .coalesce(1)
+      .write.format("com.databricks.spark.csv")
+      .mode("overwrite")
+      .option("header", "true")
+      .save("nasdaq_ma180_move")
+
+    val universeDf = move30Df.select("symbolToCopy")
+      .union(move90Df.select("symbolToCopy"))
+      .union(move180Df.select("symbolToCopy"))
+      .distinct()
+
+    universeDf
+      .coalesce(1)
+      .write.format("com.databricks.spark.csv")
+      .mode("overwrite")
+      .option("header", "true")
+      .save("nasdaq_combine")
+
+    //Screener for narrow candles
+    universeDf_Narrow.as("n")
+      .join(universeDf.as("u"), Seq("symbolToCopy"), "inner").select($"u.symbolToCopy")
+      .coalesce(1)
+      .write
+      .format("com.databricks.spark.csv")
+      .mode("overwrite")
+      .option("header", "true")
+      .save("nasdaq_narrow_candles")
+
+    //Screener with candle pattern
+    val nasdaq_today_df =
+      nasdaq_summary_raw.withColumn("date1", to_date(col("date"), "yyyyMMdd")).drop("date")
+        .withColumn("volumeMa10", avg($"volume".cast("Double")).over(ma10WindowSpec))
+        .withColumn("volumeMa10", round($"volumeMa10", 2))
+        .orderBy($"date1".desc)
+        .withColumn("dollarVolume", $"volumeMa10" * $"close")
+        .withColumn("range", ($"high" / $"low"))
+        .withColumn("avgRange", sum($"range").over(range20WindowSpec))
+        .withColumn("adr", (($"avgRange".cast("Double") / 20) - 1) * 100)
+        .withColumn("symbolToCopy", concat(lit("NASDAQ:"), $"symbol", lit(",")))
+        .filter($"dollarVolume" > 500000 and $"date1" === today)
+        .cache()
+
+    val nasdaq_yesterday_df =
+      nasdaq_summary_raw.withColumn("date1", to_date(col("date"), "yyyyMMdd")).drop("date")
+        .withColumn("volumeMa10", avg($"volume".cast("Double")).over(ma10WindowSpec))
+        .withColumn("volumeMa10", round($"volumeMa10", 2))
+        .orderBy($"date1".desc)
+        .withColumn("dollarVolume", $"volumeMa10" * $"close")
+        .withColumn("range", ($"high" / $"low"))
+        .withColumn("avgRange", sum($"range").over(range20WindowSpec))
+        .withColumn("adr", (($"avgRange".cast("Double") / 20) - 1) * 100)
+        .withColumn("symbolToCopy", concat(lit("NASDAQ:"), $"symbol", lit(",")))
+        .filter($"dollarVolume" > 500000 and $"date1" === yesterday)
+        .cache()
+
+    val patternDf = nasdaq_today_df.as("t")
+      .join(nasdaq_yesterday_df.as("y"), $"t.symbol" === $"y.symbol")
+      .filter($"y.close" > $"t.open" && $"y.open" - $"y.close" > 0
+        && $"t.close" >= ($"y.close" + ($"y.open" - $"y.close") / 2))
+      .filter($"t.close" > 5 && $"t.adr" > 3)
+      .select($"t.symbolToCopy")
+
+
+    patternDf
+      .coalesce(1)
+      .write.format("com.databricks.spark.csv")
+      .mode("overwrite")
+      .option("header", "true")
+      .save("nasdaq_pattern_screener")
 
     spark.stop()
   }
--- src/main/scala/NyseMarketDataApp.scala
+++ src/main/scala/NyseMarketDataApp.scala
@@ -1,12 +1,11 @@
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.expressions.Window
-import org.apache.spark.sql.functions.{avg, col, lit, round, sum, to_date, min, max, concat}
-
-import scala.Console.println
+import org.apache.spark.sql.functions._
 
 object NyseMarketDataApp {
   def main(args: Array[String]): Unit = {
-    val today = "2024-05-03"
+    val yesterday = "2024-05-07"
+    val today = "2024-05-08"
     val spark = SparkSession.builder
       .appName("Simple Application")
       .config("spark.master", "local")
@@ -22,13 +21,17 @@ object NyseMarketDataApp {
       .load()
 
     import spark.implicits._
-    val nyse_summary = nyse_summary_raw.withColumn("date1", to_date(col("date"), "yyyyMMdd"))
+    val nyse_summary = nyse_summary_raw.withColumn("date1", to_date(col("date"), "yyyyMMdd")).drop("date")
     val ma10WindowSpec = Window.partitionBy($"symbol").orderBy($"date1".asc).rowsBetween(-9, 0)
     val ma20WindowSpec = Window.partitionBy($"symbol").orderBy($"date1".asc).rowsBetween(-19, 0)
     val ma50WindowSpec = Window.partitionBy($"symbol").orderBy($"date1".asc).rowsBetween(-49, 0)
+    val monthWindowSpec = Window.partitionBy($"symbol").orderBy($"date1".asc).rowsBetween(-29, 0)
+    val twoMonthWindowSpec = Window.partitionBy($"symbol").orderBy($"date1".asc).rowsBetween(-59, 0)
+    val threeMonthWindowSpec = Window.partitionBy($"symbol").orderBy($"date1".asc).rowsBetween(-89, 0)
+    val sixMonthWindowSpec = Window.partitionBy($"symbol").orderBy($"date1".asc).rowsBetween(-179, 0)
     val range20WindowSpec = Window.partitionBy($"symbol").orderBy($"date1".asc).rowsBetween(-19, 0)
     val nyse_summary_df =
-      nyse_summary.drop("date")
+      nyse_summary
         .withColumn("ma10", avg($"close".cast("Double")).over(ma10WindowSpec))
         .withColumn("ma10", round($"ma10", 2))
         .withColumn("ma20", avg($"close".cast("Double")).over(ma20WindowSpec))
@@ -44,42 +47,150 @@ object NyseMarketDataApp {
         .withColumn("adr", (($"avgRange".cast("Double") / 20) - 1) * 100)
         .withColumn("min10", min($"close").over(ma10WindowSpec))
         .withColumn("max10", max($"close").over(ma10WindowSpec))
+        .withColumn("pct10d", (($"max10" - $"min10") / $"min10") * 100)
+        .withColumn("min20", min($"close").over(ma20WindowSpec))
+        .withColumn("max20", max($"close").over(ma20WindowSpec))
+        .withColumn("pct20d", (($"max20" - $"min20") / $"min20") * 100)
+        .withColumn("min30", min($"close").over(monthWindowSpec))
+        .withColumn("max30", max($"close").over(monthWindowSpec))
+        .withColumn("pct30d", (($"max30" - $"min30") / $"min30") * 100)
+        .withColumn("min60", min($"close").over(twoMonthWindowSpec))
+        .withColumn("max60", max($"close").over(twoMonthWindowSpec))
+        .withColumn("pct60d", (($"max60" - $"min60") / $"min60") * 100)
+        .withColumn("min90", min($"close").over(threeMonthWindowSpec))
+        .withColumn("max90", max($"close").over(threeMonthWindowSpec))
+        .withColumn("pct90d", (($"max90" - $"min90") / $"min90") * 100)
+        .withColumn("min180", min($"close").over(sixMonthWindowSpec))
+        .withColumn("max180", max($"close").over(sixMonthWindowSpec))
+        .withColumn("pct180d", (($"max180" - $"min180") / $"min180") * 100)
         .withColumn("symbolToCopy", concat(lit("nyse:"), $"symbol", lit(",")))
         .filter($"dollarVolume" > 500000 and $"date1" === today)
-    nyse_summary_df.show(10)
-    println("Total count greater than 500000 dollar volume ", nyse_summary_df.count())
-    //    val filtered_nyse_summary_df = nyse_summary_df
-    //      .filter($"close" >= $"ma10" && $"close" >= $"ma20" && $"close" >= $"ma50")
-    //      .filter($"close" >= 5)
-    //    println("Total stocks close greater than all moving averages ", filtered_nyse_summary_df.count())
-    //
-    //    filtered_nyse_summary_df.coalesce(1)
-    //      .write.format("com.databricks.spark.csv")
-    //      .mode("overwrite")
-    //      .option("header", "true")
-    //      .save("nyse_gt_ma")
-    //
-    //    filtered_nyse_summary_df.filter($"adr" > 4)
-    //      .coalesce(1)
-    //      .write.format("com.databricks.spark.csv")
-    //      .mode("overwrite")
-    //      .option("header", "true")
-    //      .save("nyse_adr_gt_four")
+        .cache()
 
-    //val ma10MinMaxWindowSpec = Window.partitionBy($"symbol", $"close").orderBy($"date1".asc).rowsBetween(-9, 0)
-    val move20Df = nyse_summary_df
-      .withColumn("pct10d", (($"max10" - $"min10") / $"min10") * 100)
+    val move10Df = nyse_summary_df
       .filter(($"close" > ($"min10" + ($"min10" * 0.10))) && ($"pct10d" > 20) && ($"adr" >=4) && ($"close" >= 5))
+    val universeDf_Narrow = nyse_summary_df
+      .filter(($"stockpctchange" > -1 && $"stockpctchange" < 1) && ($"adr" >=4) && ($"close" >= 5))
+    val move20Df = nyse_summary_df
+      .filter(($"close" > ($"min20" + ($"min20" * 0.10))) && ($"pct20d" > 20) && ($"adr" >=4) && ($"close" >= 5))
+    val move30Df = nyse_summary_df
+      .filter(($"close" > ($"min30" + ($"min30" * 0.30))) && ($"pct30d" > 30) && ($"adr" >=4) && ($"close" >= 5))
+    val move60Df = nyse_summary_df
+      .filter(($"close" > ($"min60" + ($"min60" * 0.20))) && ($"pct60d" > 30) && ($"adr" >=4) && ($"close" >= 5))
+    val move90Df = nyse_summary_df
+      .filter(($"close" > ($"min90" + ($"min90" * 0.40))) && ($"pct90d" > 40) && ($"adr" >=4) && ($"close" >= 5))
+    val move180Df = nyse_summary_df
+      .filter(($"close" > ($"min180" + ($"min180" * 0.50))) && ($"pct180d" > 50) && ($"adr" >=4) && ($"close" >= 5))
 
-    move20Df.show(10)
 
-    move20Df
+    move10Df
       .coalesce(1)
       .write.format("com.databricks.spark.csv")
       .mode("overwrite")
       .option("header", "true")
-      .save("nyse_ma20_move")
+      .save("nyse_ma10_move")
+
+    move30Df
+      .coalesce(1)
+      .write.format("com.databricks.spark.csv")
+      .mode("overwrite")
+      .option("header", "true")
+      .save("nyse_ma30_move")
+
+    move90Df
+      .coalesce(1)
+      .write.format("com.databricks.spark.csv")
+      .mode("overwrite")
+      .option("header", "true")
+      .save("nyse_ma90_move")
+
+    move180Df
+      .coalesce(1)
+      .write.format("com.databricks.spark.csv")
+      .mode("overwrite")
+      .option("header", "true")
+      .save("nyse_ma180_move")
+
+    val universeDf = move30Df.select("symbolToCopy")
+      .union(move90Df.select("symbolToCopy"))
+      .union(move180Df.select("symbolToCopy"))
+      .distinct()
+
+    universeDf
+      .coalesce(1)
+      .write.format("com.databricks.spark.csv")
+      .mode("overwrite")
+      .option("header", "true")
+      .save("nyse_combine")
+
+    //Screener for narrow candles
+    universeDf_Narrow.as("n")
+      .join(universeDf.as("u"), Seq("symbolToCopy"), "inner").select($"u.symbolToCopy")
+      .coalesce(1)
+      .write.format("com.databricks.spark.csv")
+      .mode("overwrite")
+      .option("header", "true")
+      .save("nyse_narrow_candles")
+
+    //Screener with candle pattern
+    val nyse_today_df =
+      nyse_summary
+        .withColumn("volumeMa10", avg($"volume".cast("Double")).over(ma10WindowSpec))
+        .withColumn("volumeMa10", round($"volumeMa10", 2))
+        .orderBy($"date1".desc)
+        .withColumn("dollarVolume", $"volumeMa10" * $"close")
+        .withColumn("range", ($"high" / $"low"))
+        .withColumn("avgRange", sum($"range").over(range20WindowSpec))
+        .withColumn("adr", (($"avgRange".cast("Double") / 20) - 1) * 100)
+        .withColumn("symbolToCopy", concat(lit("nyse:"), $"symbol", lit(",")))
+        .filter($"dollarVolume" > 500000 and $"date1" === today)
+        .cache()
+
+    val nyse_yesterday_df =
+      nyse_summary
+        .withColumn("volumeMa10", avg($"volume".cast("Double")).over(ma10WindowSpec))
+        .withColumn("volumeMa10", round($"volumeMa10", 2))
+        .orderBy($"date1".desc)
+        .withColumn("dollarVolume", $"volumeMa10" * $"close")
+        .withColumn("range", ($"high" / $"low"))
+        .withColumn("avgRange", sum($"range").over(range20WindowSpec))
+        .withColumn("adr", (($"avgRange".cast("Double") / 20) - 1) * 100)
+        .withColumn("symbolToCopy", concat(lit("nyse:"), $"symbol", lit(",")))
+        .filter($"dollarVolume" > 500000 and $"date1" === yesterday)
+        .cache()
+
+    nyse_today_df.as("t")
+      .join(nyse_yesterday_df.as("y"), Seq("symbol"))
+      .filter($"y.close" > $"t.open" && $"y.open" - $"y.close" > 0
+        && $"t.close" >= ($"y.close" + ($"y.open" - $"y.close") / 2))
+      .filter($"t.close" > 5 && $"t.adr" > 3)
+      .select($"t.symbolToCopy")
+      .coalesce(1)
+      .write.format("com.databricks.spark.csv")
+      .mode("overwrite")
+      .option("header", "true")
+      .save("nyse_pattern_screener")
 
     spark.stop()
   }
 }
+
+//    val filtered_nyse_summary_df = nyse_summary_df
+//      .filter($"close" >= $"ma10" && $"close" >= $"ma20" && $"close" >= $"ma50")
+//      .filter($"close" >= 5)
+//    println("Total stocks close greater than all moving averages ", filtered_nyse_summary_df.count())
+//
+//    filtered_nyse_summary_df.coalesce(1)
+//      .write.format("com.databricks.spark.csv")
+//      .mode("overwrite")
+//      .option("header", "true")
+//      .save("nyse_gt_ma")
+//
+//    filtered_nyse_summary_df.filter($"adr" > 4)
+//      .coalesce(1)
+//      .write.format("com.databricks.spark.csv")
+//      .mode("overwrite")
+//      .option("header", "true")
+//      .save("nyse_adr_gt_four")
+
+//val ma10MinMaxWindowSpec = Window.partitionBy($"symbol", $"close").orderBy($"date1".asc).rowsBetween(-9, 0)
